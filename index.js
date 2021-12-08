5a5307fe33c76888cdd554b36fb7037803c1c23b
gary <gary.white@lodgit.com>
2021-12-08 13:29:43 +0100

    boilerplate

---
+++ index.js
@@ -0,0 +1,9 @@
+function twiceAsOld(dadYearsOld, sonYearsOld) {
+    // your code here
+}
+
+console.log(twiceAsOld(36,7)); // Return 22
+console.log(twiceAsOld(55,30)); // Return 5
+console.log(twiceAsOld(42,21)); // Return 0
+console.log(twiceAsOld(22,1)); // Return 20
+console.log(twiceAsOld(29,0)); // Return 29
